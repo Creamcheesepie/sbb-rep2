d5f9543124e8ca7b4b482d0165e979a982f3d964
Creamcheesepie <gnldbs1004@naver.com>
2025-09-19 08:50:13 +0900

    최초 커밋

---
+++ src/main/java/com/mysite/sbb/SbbRep2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbRep2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbRep2Application.class, args);
+    }
+
+}
