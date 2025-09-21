265ed01dba4091f0fd98e8d6209d953b03b41eb3
Creamcheesepie <gnldbs1004@naver.com>
2025-09-21 11:03:36 +0900

    상세조회 페이지 제작

---
+++ src/main/java/com/mysite/sbb/DataNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mysite.sbb;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason="entity not found")
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
